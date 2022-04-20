8ea98e376519eff30d44ad55e0a9cbd9934e8d56
milyd <85845327+milyd@users.noreply.github.com>
2022-04-20 22:28:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container")
+
+function show() {
+    container.innerHTML = "<h1>About me</h1>My name is Dominik. I live in Poland, Ostroleka.<br> I can program in this programming languages: <b>PHP, JS, Python</b><br><br>My phone: <b>iPhone XR</b>"
+}
