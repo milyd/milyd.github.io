be97a79047f5526a09b33365a5b7665890a7dd4d
milyd <85845327+milyd@users.noreply.github.com>
2022-07-08 13:36:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById("container")
 
 function show() {
-    container.innerHTML = "<h1>About me</h1>My name is Dominik. I live in Poland, Ostroleka.<br> I can program in this programming languages: <b>PHP, JS, Python</b><br><br>My phone: <b>iPhone XR</b>"
+    container.innerHTML = "<h1>About me</h1>My name is Dominik. I live in Poland, Ostroleka.<br> I can program in this programming languages: <b>PHP, JS, Python</b><br><br>My phone: <b>Samsung Galaxy A33 5G</b>"
 }
